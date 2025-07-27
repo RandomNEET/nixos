3dd2095658e4343bb6cf83bfc732407a358545b3
RandomNEET <dev@randomneet.me>
2025-07-27 16:57:55 +0800

    remove config for /nix

--- modules/snapper/default.nix
+++ modules/snapper/default.nix
@@ -2,12 +2,6 @@
 {
   services.snapper = {
     configs = {
-      nix = {
-        SUBVOLUME = "/nix";
-        ALLOW_USERS = [ "${opts.username}" ];
-        TIMELINE_CREATE = true;
-        TIMELINE_CLEANUP = true;
-      };
       home = {
         SUBVOLUME = "/home";
         ALLOW_USERS = [ "${opts.username}" ];
