b85d5cd180f176915a0ff36742e54fcf3ad75ebe
RandomNEET <dev@randomneet.me>
2025-07-27 16:58:06 +0800

    encrypt .vault folder

--- modules/scripts/user/dix/backup.nix
+++ modules/scripts/user/dix/backup.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "backup" ''
-  rsync -avz --delete ~/nixos /mnt/hdd2/backup
-  rsync -avz --delete ~/.vault /mnt/hdd2/backup
-  rsync -avz --delete ~/doc /mnt/hdd2/backup
-  rsync -avz --delete ~/pic /mnt/hdd2/backup
-  rsync -avz --delete ~/pkg /mnt/hdd2/backup
-  rsync -avz --delete ~/repo /mnt/hdd2/backup
+  tar czf vault.tar.gz ~/.vault
+  gpg -e -r 349FEC12317B400EE0570F6648D4C0CF3B64D33F vault.tar.gz && rm vault.tar.gz
+  mv -v ~/vault.tar.gz.gpg /mnt/hdd2/backup
+
+  rsync -av --delete ~/nixos /mnt/hdd2/backup
+  rsync -av --delete ~/doc /mnt/hdd2/backup
+  rsync -av --delete ~/pic /mnt/hdd2/backup
+  rsync -av --delete ~/pkg /mnt/hdd2/backup
+  rsync -av --delete ~/repo /mnt/hdd2/backup
 ''
--- modules/scripts/user/nasix/backup.nix
+++ modules/scripts/user/nasix/backup.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "backup" ''
-  rsync -avz --delete ~/nixos /mnt/smb/backup/nixos
-  rsync -avz --delete /var/lib/jellyfin /mnt/smb/backup/jellyfin/config
-  rsync -avz --delete /var/cache/jellyfin /mnt/smb/backup/jellyfin/cache
-  sudo rsync -avz --delete ~/.docker /mnt/smb/backup/docker
+  rsync -av --delete ~/nixos /mnt/ssd/backup/nixos
+  rsync -av --delete /var/lib/jellyfin /mnt/ssd/backup/jellyfin/config
+  rsync -av --delete /var/cache/jellyfin /mnt/ssd/backup/jellyfin/cache
+  sudo rsync -av --delete ~/.docker /mnt/ssd/backup/docker
 ''
