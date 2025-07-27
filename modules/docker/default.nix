03281ec95b2ca70ec921b62056c21356c4f488d0
RandomNEET <dev@randomneet.me>
2025-07-27 16:58:01 +0800

    remove nvidia toolkit

--- modules/docker/default.nix
+++ modules/docker/default.nix
@@ -3,13 +3,6 @@
   virtualisation.docker = {
     enable = true;
   };
-  hardware.nvidia-container-toolkit =
-    if opts.gpu == "nvidia" then
-      {
-        enable = true;
-      }
-    else
-      { };
   users = {
     users = {
       ${opts.username} = {
