0a08ed8cba96ab02405f7d3d5a7678f35c3569d8
RandomNEET <dev@randomneet.me>
2025-08-27 05:44:51 +0800

    feat(spicetify): update theme and add custom apps

--- modules/spicetify/default.nix
+++ modules/spicetify/default.nix
@@ -7,29 +7,19 @@
         spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
       in
       {
-        # import the flake's module for your system
         imports = [ inputs.spicetify-nix.homeManagerModules.default ];
-        # configure spicetify :)
+
         programs.spicetify = {
           enable = true;
-          theme = spicePkgs.themes.text;
-          colorScheme = "CatppuccinMocha";
+          theme = spicePkgs.themes.comfy;
+          colorScheme = "catppuccin-mocha";
           enabledExtensions = with spicePkgs.extensions; [
-            # adblock
-            shuffle # shuffle+ (special characters are sanitized out of ext names)
-            keyboardShortcut # vimium-like navigation
-            # copyLyrics # copy lyrics with selection
-            # autoVolume
-            # showQueueDuration
-            # fullAppDisplay
-            # hidePodcasts
+            shuffle
+            keyboardShortcut
           ];
           enabledCustomApps = with spicePkgs.apps; [
-            # reddit
-            # lyricsPlus
-            # marketplace
-            # localFiles
-            # ncsVisualizer
+            lyricsPlus
+            historyInSidebar
           ];
         };
       }
