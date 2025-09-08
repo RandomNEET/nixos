44614f304a8144c9d04d1b32ce5e7714536cd3a6
RandomNEET <dev@randomneet.me>
2025-09-09 01:08:06 +0800

    add host lix

---
+++ hosts/lix/configuration.nix
@@ -0,0 +1,100 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  lib,
+  pkgs,
+  opts,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../common.nix
+
+    ../../modules/bash
+    ../../modules/bat
+    ../../modules/blueman
+    ../../modules/btop
+    ../../modules/cava
+    # ../../modules/cron
+    # ../../modules/docker
+    ../../modules/direnv
+    ../../modules/discord
+    ../../modules/disks
+    ../../modules/eza
+    ../../modules/fastfetch
+    ../../modules/firefox
+    ../../modules/firejail
+    ../../modules/flatpak
+    ../../modules/fonts
+    # ../../modules/frp
+    ../../modules/fzf
+    ../../modules/games
+    ../../modules/garbage-collection
+    ../../modules/gh
+    # ../../modules/gimp
+    ../../modules/git
+    ../../modules/gpg
+    #../../modules/gpu
+    ../../modules/greetd
+    ../../modules/impermanence
+    # ../../modules/jellyfin
+    ../../modules/jq
+    ../../modules/kitty
+    ../../modules/lazygit
+    ../../modules/libreoffice
+    ../../modules/localsend
+    ../../modules/mpv
+    ../../modules/nixvim
+    ../../modules/obs-studio
+    ../../modules/obsidian
+    ../../modules/pipewire
+    ../../modules/proxy
+    ../../modules/rice
+    ../../modules/rigprep
+    # ../../modules/samba
+    ../../modules/scripts
+    #    ../../modules/secure-boot
+    ../../modules/snapper
+    ../../modules/spicetify
+    ../../modules/spotify-player
+    ../../modules/ssh
+    ../../modules/swayimg
+    ../../modules/thunderbird
+    ../../modules/tmux
+    ../../modules/trash-cli
+    ../../modules/utilities
+    ../../modules/virtualisation
+    ../../modules/vscode
+    ../../modules/yazi
+    ../../modules/zathura
+    ../../modules/zoxide
+    ../../modules/zsh
+
+    ../../modules/desktop/hyprland
+    ../../modules/desktop/themes/catppuccin
+  ];
+
+  # Define system packages here
+  environment.systemPackages = with pkgs; [ ];
+
+  # Home-manager config
+  home-manager.sharedModules = [
+    (_: {
+      home.packages = with pkgs; [ ];
+    })
+  ];
+
+  # Kernel
+  boot.kernelPackages = pkgs.linuxPackages_zen; # _latest, _zen, _xanmod_latest, _hardened, _rt, _OTHER_CHANNEL, etc
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+}
---
+++ hosts/lix/options.nix
@@ -0,0 +1,247 @@
+rec {
+  hostname = "lix";
+  system = "x86_64-linux";
+
+  gpu = "";
+  locale = "en_US.UTF-8";
+  timezone = "Asia/Shanghai";
+  kbdLayout = "us";
+  kbdVariant = "";
+  consoleKeymap = "us";
+  rootpasswd = "$6$1bNtqKFsObhMC1OG$THnog0HqmR/GnN.0IwndZzuijVMiV0cZIPUjmCvDs6gsjHAc.FYfcIlKmiMx2hy2gbd814Br1uNAhiyKl4W9g.";
+
+  username = "howl";
+  userpasswd = "$6$.FVrKngH1eXjNYi9$lsTAUQvvJyB209fhkf3g5E12iCcgNdDZKW0XTwCp7i3lNwM8gjNq3kRgjW4WIBV68YETysoDCHhKtSIncPT3n1";
+  editor = "nvim";
+  terminal = "kitty";
+  terminalFileManager = "yazi";
+  browser = "firefox";
+  emailClient = "thunderbird";
+  xdg = {
+    userDirs = {
+      desktop = null; # null or "$HOME/dsk"
+      documents = "$HOME/doc";
+      download = "$HOME/dls";
+      music = "$HOME/mus";
+      pictures = "$HOME/pic";
+      publicShare = "$HOME/pub";
+      templates = "$HOME/tpl";
+      videos = "$HOME/vid";
+    };
+  };
+
+  # https://github.com/nix-community/lanzaboote/blob/master/docs/QUICK_START.md
+  secure-boot = {
+    enable = true;
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/nix/persist/var/lib/sbctl";
+    };
+  };
+
+  snapper = {
+    config = {
+      home = {
+        SUBVOLUME = "/home";
+        ALLOW_USERS = [ "${username}" ];
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+      };
+    };
+    snapshotInterval = "hourly";
+    cleanupInterval = "1d";
+  };
+
+  ssh = {
+    dir = "/home/${username}/.vault/ssh";
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "${ssh.dir}/gh-randomneet";
+        addKeysToAgent = "yes";
+      };
+      "dix" = {
+        hostname = "192.168.0.24";
+        port = 22;
+        user = "howl";
+        identityFile = "${ssh.dir}/dix";
+        addKeysToAgent = "yes";
+      };
+      "nasix" = {
+        hostname = "192.168.0.56";
+        port = 22;
+        user = "howl";
+        identityFile = "${ssh.dir}/nasix";
+        addKeysToAgent = "yes";
+      };
+    };
+    daemon = {
+      enable = true;
+      ports = [
+        22
+      ];
+      authorizedKeysFiles = [ "${ssh.dir}/dix.pub" ];
+      settings = {
+        PasswordAuthentication = false;
+      };
+    };
+    agent.enable = false;
+  };
+
+  gpg = {
+    homedir = "/home/${username}/.gnupg";
+    agent = {
+      enable = true;
+      enableSshSupport = true; # Automatically disable ssh-agent if set to true
+    };
+  };
+
+  proxy = {
+    method = "tproxy"; # tproxy lpf
+    settingsFile = "/home/${username}/.vault/proxy/${proxy.method}/outsider/default.json";
+  };
+
+  zsh = {
+    initContent = '''';
+
+    envExtra = ''
+      export VI_MODE_SET_CURSOR=true
+      MODE_INDICATOR="%F{red}<<<%f"
+    '';
+
+    shellGlobalAliases = {
+      G = "| grep";
+    };
+    shellAliases = {
+      update = "sudo nixos-rebuild switch";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "vi-mode"
+      ];
+      theme = "simple";
+    };
+  };
+
+  nixvim = {
+    withNodeJs = false;
+    withPerl = false;
+    withPython3 = true;
+    withRuby = false;
+
+    lsp.enable = true;
+    treesitter.enable = true; # Automatically disable noice if set to false
+    lint.enable = true;
+    conform.enable = true;
+    copilot.enable = true;
+    noice.enable = true; # Require treesitter
+    snack = {
+      image.enable = true;
+    };
+    obsidian = {
+      workspaces = [
+        {
+          name = "index";
+          path = "~/${obsidian.vaults.default.target}/00 index";
+          overrides = {
+            notes_subdir = "00 index";
+          };
+        }
+        {
+          name = "study";
+          path = "~/${obsidian.vaults.default.target}/10 study";
+          overrides = {
+            notes_subdir = "10 study";
+          };
+        }
+        {
+          name = "develop";
+          path = "~/${obsidian.vaults.default.target}/20 develop";
+          overrides = {
+            notes_subdir = "20 develop";
+          };
+        }
+        {
+          name = "daily";
+          path = "~/${obsidian.vaults.default.target}/30 daily";
+          overrides = {
+            notes_subdir = "30 daily";
+          };
+        }
+        {
+          name = "todos";
+          path = "~/${obsidian.vaults.default.target}/99 todos";
+          overrides = {
+            notes_subdir = "99 todos";
+          };
+        }
+      ];
+    };
+  };
+
+  git = {
+    userName = "RandomNEET";
+    userEmail = "dev@randomneet.me";
+  };
+
+  wallpaper = {
+    dir = "/home/${username}/pic/wallpapers";
+    default = "${wallpaper.dir}/universal/black.png";
+  };
+
+  hyprland = {
+    monitor = [
+      "desc:Chimei Innolux Corporation 0x14C9, 1920x1080@60, 0x0, 1"
+    ];
+    workspaceBind = ''
+      workspace = 1, monitor:desc:Chimei Innolux Corporation 0x14C9, default:true;
+    '';
+  };
+
+  hyprlock = {
+    monitor1 = "eDP-1";
+    monitor2 = "";
+    background1 = "${wallpaper.dir}/horizontal/mocha/greenbus.jpg";
+  };
+
+  hypridle = {
+    time = {
+      lock = "300";
+      dpmsOff = "";
+      suspend = "";
+    };
+  };
+
+  obsidian = {
+    vaults = {
+      default = {
+        enable = true;
+        target = "doc/notes";
+      };
+    };
+  };
+
+  flatpak = {
+    packages = {
+      system = [ ];
+      user = [
+        "com.github.tchx84.Flatseal"
+        "com.qq.QQ"
+        "com.tencent.WeChat"
+      ];
+    };
+  };
+
+  samba = {
+    settings = {
+    };
+  };
+
+  frp = {
+    role = ""; # server client
+    settings = {
+    };
+  };
+}
