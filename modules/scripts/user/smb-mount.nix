ad140c11529804cabae03dc64c8d2de325f5eaf2
RandomNEET <dev@randomneet.me>
2025-09-10 05:51:16 +0800

    update script

--- modules/scripts/user/smb-mount.nix
+++ modules/scripts/user/smb-mount.nix
@@ -1,10 +1,10 @@
 { pkgs, opts, ... }:
-if opts.hostname != null && opts.hostname == "dix" then
+if opts.hostname != null && (opts.hostname == "dix" || opts.hostname == "lix") then
   pkgs.writeShellScriptBin "smb-mount" ''
     IP="192.168.0.56"
 
     if [ ! -d "/mnt/smb" ]; then
-    	sudo mkdir /mnt/smb
+    	sudo mkdir -p /mnt/smb
     fi
 
     sudo mount -t cifs //"$IP"/private /mnt/smb -o username=$USER,uid=$(id -u),gid=$(id -g)
