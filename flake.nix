c674cfd081069477be74b28f74f330b6269fde45
RandomNEET <dev@randomneet.me>
2025-11-01 18:43:55 +0800

    overlay stable pkgs and add formatter

--- flake.nix
+++ flake.nix
@@ -1,19 +1,16 @@
 {
   description = "howl's flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nur.url = "github:nix-community/NUR";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
     nixvim = {
       url = "github:nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     betterfox = {
@@ -25,49 +22,64 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote?ref=v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
   };
   outputs =
-    inputs@{ nixpkgs, home-manager, ... }:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
     let
-      hosts = builtins.filter (name: builtins.pathExists ./hosts/${name}/options.nix) (
+      hosts = builtins.filter (n: builtins.pathExists (./hosts + "/${n}/options.nix")) (
         builtins.attrNames (builtins.readDir ./hosts)
       );
+      overlay = (
+        final: prev:
+        let
+          stable = inputs."nixpkgs-stable".legacyPackages.${final.system};
+        in
+        (import ./pkgs { pkgs = final; })
+        // {
+          inherit stable;
+        }
+      );
+      mkNixos =
+        name:
+        let
+          hostPath = ./hosts + "/${name}";
+          opts = import (hostPath + "/options.nix");
+        in
+        {
+          name = opts.hostname;
+          value = nixpkgs.lib.nixosSystem {
+            system = opts.system;
+            specialArgs = { inherit inputs opts; };
+            modules = [
+              hostPath
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.extraSpecialArgs = { inherit inputs opts; };
+              }
+              { nixpkgs.overlays = [ overlay ]; }
+            ];
+          };
+        };
     in
     {
-      nixosConfigurations = builtins.listToAttrs (
-        map (
-          name:
-          let
-            opts = import ./hosts/${name}/options.nix;
-          in
-          {
-            name = opts.hostname;
-            value = nixpkgs.lib.nixosSystem {
-              system = opts.system;
-              specialArgs = {
-                inherit inputs opts;
-              };
-              modules = [
-                ./hosts/${name}
-                home-manager.nixosModules.home-manager
-                {
-                  home-manager.extraSpecialArgs = {
-                    inherit opts;
-                  };
-                }
-                {
-                  nixpkgs.overlays = [
-                    (final: prev: import ./pkgs { pkgs = final; })
-                  ];
-                }
-              ];
-            };
-          }
-        ) hosts
+      nixosConfigurations = nixpkgs.lib.listToAttrs (map mkNixos hosts);
+      formatter = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt-tree
       );
     };
 }
