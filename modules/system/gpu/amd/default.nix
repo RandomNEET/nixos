e388a1818039676f9112607df0416315e00dd50f
RandomNEET <dev@randomneet.me>
2025-11-08 00:58:04 +0800

    add amd gpu

---
+++ modules/system/gpu/amd/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "amdgpu" ];
+  };
+  environment.systemPackages = with pkgs; [ rocmPackages.amdsmi ];
+  hardware.amdgpu = {
+    opencl.enable = true;
+  };
+}
