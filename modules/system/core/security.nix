2cde1de3a963089380cdb62e5a8d9ed4ff7abc5f
RandomNEET <dev@randomneet.me>
2025-11-08 22:44:38 +0800

    add options

--- modules/system/core/security.nix
+++ modules/system/core/security.nix
@@ -1,9 +1,18 @@
 { opts, ... }:
 {
-  security.sudo = {
-    extraConfig = ''
-      Defaults lecture = never
-    ''
-    + (opts.sudo.extraConfig or "");
+  security = {
+    sudo = {
+      enable = true;
+      keepTerminfo = opts.sudo.keepTerminfo or true;
+      execWheelOnly = opts.sudo.execWheelOnly or false;
+      wheelNeedsPassword = opts.sudo.wheelNeedsPassword or true;
+      configFile = ''
+        Defaults lecture = never
+      ''
+      + (opts.sudo.configFile or "");
+      defaultOptions = [ "SETENV" ] ++ (opts.sudo.defaultOptions or [ ]);
+      extraConfig = '''' + (opts.sudo.extraConfig or "");
+      extraRules = [ ] ++ (opts.sudo.extraRules or [ ]);
+    };
   };
 }
