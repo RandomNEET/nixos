e7673543ca8604c28d03765bc29ca18536b5db1a
RandomNEET <dev@randomneet.me>
2025-11-08 23:09:13 +0800

    update fastwarp config

--- modules/programs/cli/nixvim/plugins/nvim-autopairs.nix
+++ modules/programs/cli/nixvim/plugins/nvim-autopairs.nix
@@ -9,8 +9,23 @@
           "snacks_picker_input"
         ];
         fast_wrap = {
-          end_key = "$";
           map = "<M-e>";
+          chars = [
+            "{"
+            "["
+            "("
+            "\""
+            "'"
+          ];
+          pattern = "[[%'%\"%>%]%)%}%,]]";
+          end_key = "$";
+          before_key = "h";
+          after_key = "l";
+          cursor_pos_before = true;
+          keys = "qwertyuiopzxcvbnmasdfghjkl";
+          manual_position = true;
+          highlight = "Search";
+          highlight_grey = "Comment";
         };
       };
     };
