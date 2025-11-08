f1d0ef4519b940741af3357b75c63465e380e5df
RandomNEET <dev@randomneet.me>
2025-11-08 19:36:53 +0800

    smart case

--- modules/programs/cli/nixvim/core/options.nix
+++ modules/programs/cli/nixvim/core/options.nix
@@ -2,8 +2,18 @@
   programs.nixvim = {
     config = {
       opts = {
+        ignorecase = true;
+        smartcase = true;
+        hlsearch = true;
+        incsearch = true;
+
+        wildmenu = true;
+        wildmode = "longest:full,full";
+        wildignorecase = true;
+
         number = true;
         relativenumber = true;
+
         splitright = true;
         splitbelow = true;
       };
