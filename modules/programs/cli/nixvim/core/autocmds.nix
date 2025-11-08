452d0d9ea491973936851e7337b795b2d0309858
RandomNEET <dev@randomneet.me>
2025-11-08 19:37:06 +0800

    foldexpr

--- modules/programs/cli/nixvim/core/autocmds.nix
+++ modules/programs/cli/nixvim/core/autocmds.nix
@@ -2,6 +2,26 @@
   programs.nixvim = {
     autoCmd = [
       {
+        event = "BufReadPost";
+        pattern = "*";
+        callback = {
+          __raw = ''
+            function()
+              vim.schedule(function()
+                if vim.wo.foldmethod ~= 'manual' and vim.wo.foldmethod ~= ''' then
+                  return
+                end
+                
+                vim.opt_local.foldmethod = 'expr'
+                vim.opt_local.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.opt_local.foldlevel = 99
+              end)
+            end
+          '';
+        };
+      }
+
+      {
         event = "FileType";
         pattern = "markdown";
         callback = {
