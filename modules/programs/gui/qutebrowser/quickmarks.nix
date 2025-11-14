b1b7b4f734a1bf928721bdfc223193c846dd87c5
RandomNEET <dev@randomneet.me>
2025-11-15 02:57:39 +0800

    update quickmarks

--- modules/programs/gui/qutebrowser/quickmarks.nix
+++ modules/programs/gui/qutebrowser/quickmarks.nix
@@ -1,15 +1,15 @@
 { opts, ... }:
 (
   {
-    ib = "https://webmail.migadu.com/";
+    em = "https://webmail.migadu.com/";
     gh = "https://github.com/";
     yt = "https://www.youtube.com/";
     bi = "https://bilibili.com/";
     rd = "https://www.reddit.com/";
     tw = "https://twitter.com/";
     st = "https://store.steampowered.com/";
-    cld = "https://claude.ai/";
     gmn = "https://gemini.google.com/";
+    cld = "https://claude.ai/";
     gpt = "https://chatgpt.com/";
     cop = "https://copilot.github.com/";
   }
