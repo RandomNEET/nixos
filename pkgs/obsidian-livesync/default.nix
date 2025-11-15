64490886f785e84f8af6d43042e4d513f98c99a0
RandomNEET <dev@randomneet.me>
2025-11-16 02:32:42 +0800

    update to 0.25.27

--- pkgs/obsidian-livesync/default.nix
+++ pkgs/obsidian-livesync/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation {
   pname = "obsidian-livesync";
-  version = "0.25.12";
+  version = "0.25.27";
   src = null;
   dontUnpack = true;
 
@@ -9,22 +9,22 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out
     cp ${
       pkgs.fetchurl {
-        url = "https://github.com/vrtmrz/obsidian-livesync/releases/download/0.25.12/main.js";
-        sha256 = "0134e98953c0e29024c58821b70344a862fdee82eefe9550160677e2bfbeb1eb";
+        url = "https://github.com/vrtmrz/obsidian-livesync/releases/download/0.25.27/main.js";
+        sha256 = "sha256-qgh7hJG0bZ3q39m/YjtfVh6hzvmJ8AlSUQk0vfW+z6o=";
       }
     } $out/main.js
 
     cp ${
       pkgs.fetchurl {
-        url = "https://github.com/vrtmrz/obsidian-livesync/releases/download/0.25.12/manifest.json";
-        sha256 = "c3ddf0267a7765e5b62bad13b36e1905b976b472564fcc92ad9b147348d779cd";
+        url = "https://github.com/vrtmrz/obsidian-livesync/releases/download/0.25.27/manifest.json";
+        sha256 = "sha256-4THjpQbFNUk8fgrXcYbZGkEALQkDWpAXa9Xjk0kbK3g=";
       }
     } $out/manifest.json
 
     cp ${
       pkgs.fetchurl {
-        url = "https://github.com/vrtmrz/obsidian-livesync/releases/download/0.25.12/styles.css";
-        sha256 = "ec29fb248ca163f3bc0037a2d8a76ae70eb3de343747779d58fe16dd588371ca";
+        url = "https://github.com/vrtmrz/obsidian-livesync/releases/download/0.25.27/styles.css";
+        sha256 = "sha256-O9nrEIKaJ21tu1S9qRFSGeBD5bYdA/VEpByDUH0PM0U=";
       }
     } $out/styles.css
   '';
