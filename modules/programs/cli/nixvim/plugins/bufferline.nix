b3e676cfeae5e68ddacc52547dcb5d24aea1f008
RandomNEET <dev@randomneet.me>
2025-11-18 15:56:04 +0800

    edit appearance

--- modules/programs/cli/nixvim/plugins/bufferline.nix
+++ modules/programs/cli/nixvim/plugins/bufferline.nix
@@ -1,89 +1,132 @@
 {
-  programs.nixvim = {
-    keymaps = [
-      {
-        mode = "n";
-        key = "<S-l>";
-        action = "<cmd>BufferLineCycleNext<cr>";
-        options = {
-          desc = "Cycle to next buffer";
-        };
-      }
-      {
-        mode = "n";
-        key = "<S-h>";
-        action = "<cmd>BufferLineCyclePrev<cr>";
-        options = {
-          desc = "Cycle to previous buffer";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bo";
-        action = "<cmd>BufferLineCloseOthers<cr>";
-        options = {
-          desc = "Delete other buffers";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>br";
-        action = "<cmd>BufferLineCloseRight<cr>";
-        options = {
-          desc = "Delete buffers to the right";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bl";
-        action = "<cmd>BufferLineCloseLeft<cr>";
-        options = {
-          desc = "Delete buffers to the left";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bp";
-        action = "<cmd>BufferLineTogglePin<cr>";
-        options = {
-          desc = "Toggle pin";
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>bP";
-        action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
-        options = {
-          desc = "Delete non-pinned buffers";
-        };
-      }
-    ];
-    plugins.bufferline = {
-      enable = true;
-      settings = {
-        options = {
-          offsets = [
-            {
-              filetype = "snacks_layout_box";
-              text = "↓ Files     ᗜ_ᗜ     Buffers →";
-              text_align = "center";
-              separator = true;
-            }
-          ];
-          separator_style = "thin"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
-          indicator.icon = "▎";
-          modified_icon = "●";
-          buffer_close_icon = "󰅖";
-          close_icon = "";
-          left_trunc_marker = " ";
-          right_trunc_marker = " ";
-          numbers = "ordinal";
-          tab_size = 18;
-          diagnostics = "nvim_lsp";
-          show_buffer_icons = true;
-          show_buffer_close_icons = false;
+  programs.nixvim =
+    { lib, ... }:
+    let
+      cwd = lib.nixvim.mkRaw ''
+        function()
+          local full_path = vim.fn.getcwd()
+          local home = vim.fn.expand("~")
+          local max_length = 30
+          local display_path
+          
+          if full_path:sub(1, #home) == home then
+            display_path = full_path:gsub(home, "~", 1)
+          else
+            display_path = full_path
+          end
+          
+          if #display_path > max_length then
+            local parts = vim.split(display_path, "[/\\]")
+            
+            local num_parts_to_keep = 3 
+            local start_index = math.max(1, #parts - num_parts_to_keep + 1)
+            
+            local abbreviated_parts = {}
+            for i = start_index, #parts do
+              table.insert(abbreviated_parts, parts[i])
+            end
+            
+            display_path = table.concat(abbreviated_parts, "/")
+            
+            if #parts > num_parts_to_keep then
+                display_path = "…/" .. display_path
+            end
+          end
+          
+          return display_path
+        end
+      '';
+    in
+    {
+      keymaps = [
+        {
+          mode = "n";
+          key = "<S-l>";
+          action = "<cmd>BufferLineCycleNext<cr>";
+          options = {
+            desc = "Cycle to next buffer";
+          };
+        }
+        {
+          mode = "n";
+          key = "<S-h>";
+          action = "<cmd>BufferLineCyclePrev<cr>";
+          options = {
+            desc = "Cycle to previous buffer";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bo";
+          action = "<cmd>BufferLineCloseOthers<cr>";
+          options = {
+            desc = "Delete other buffers";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>br";
+          action = "<cmd>BufferLineCloseRight<cr>";
+          options = {
+            desc = "Delete buffers to the right";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bl";
+          action = "<cmd>BufferLineCloseLeft<cr>";
+          options = {
+            desc = "Delete buffers to the left";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bp";
+          action = "<cmd>BufferLineTogglePin<cr>";
+          options = {
+            desc = "Toggle pin";
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>bP";
+          action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
+          options = {
+            desc = "Delete non-pinned buffers";
+          };
+        }
+      ];
+      plugins.bufferline = {
+        enable = true;
+        settings = {
+          options = {
+            mode = "buffers";
+            style_preset = "bufferline.style_preset.default";
+            numbers = "ordinal";
+            indicator = {
+              icon = "▎";
+              style = "icon";
+            };
+            buffer_close_icon = "󰅖";
+            modified_icon = "●";
+            close_icon = "";
+            left_trunc_marker = " ";
+            right_trunc_marker = " ";
+            tab_size = 18;
+            diagnostics = "nvim_lsp";
+            offsets = [
+              {
+                filetype = "snacks_layout_box";
+                text = cwd;
+                text_align = "left";
+                separator = true;
+              }
+            ];
+            show_buffer_icons = true;
+            show_buffer_close_icons = false;
+            separator_style = "thin"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+          };
         };
       };
     };
-  };
 }
--- modules/programs/cli/nixvim/plugins/lualine.nix
+++ modules/programs/cli/nixvim/plugins/lualine.nix
@@ -13,7 +13,7 @@
             globalstatus = true;
             disabled_filetypes = {
               statusline = [
-                "snack_dashboard"
+                "snacks_dashboard"
               ];
             };
             component_separators.left = "";
@@ -29,8 +29,30 @@
           // lib.optionalAttrs ((opts.theme or "") == "catppuccin-mocha") {
             theme = "catppuccin";
           };
-          sections.lualine_c = [ "filename" ];
-          sections.lualine_x = [ "location" ];
+          sections = {
+            lualine_a = [ "mode" ];
+            lualine_b = [
+              "branch"
+              "diff"
+              "diagnostics"
+            ];
+            lualine_c = [ "filename" ];
+            lualine_x = [
+              "encoding"
+              "fileformat"
+              "filetype"
+            ];
+            lualine_y = [ "progress" ];
+            lualine_z = [ "location" ];
+          };
+          inactive_sections = {
+            lualine_a = [ ];
+            lualine_b = [ ];
+            lualine_c = [ "filename" ];
+            lualine_x = [ "location" ];
+            lualine_y = [ ];
+            lualine_z = [ ];
+          };
           tabline = { };
           winbar = { };
           inactive_winbar = { };
