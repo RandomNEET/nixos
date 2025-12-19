984ab73fe2ab2ecb420b3afbacbd5dc4e6368129
RandomNEET <dev@randomneet.me>
2025-12-20 02:42:45 +0800

    add keymaps

--- modules/programs/cli/nixvim/plugins/persistence.nix
+++ modules/programs/cli/nixvim/plugins/persistence.nix
@@ -1,5 +1,39 @@
 {
   programs.nixvim = {
+    keymaps = [
+      {
+        mode = "n";
+        action = "<cmd>lua require('persistence').load()<CR>";
+        key = "<leader>qs";
+        options = {
+          desc = "Load the session for the current directory";
+        };
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('persistence').select()<CR>";
+        key = "<leader>qS";
+        options = {
+          desc = "Select a session to load";
+        };
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('persistence').load({ last = true })<CR>";
+        key = "<leader>ql";
+        options = {
+          desc = "Load the last session";
+        };
+      }
+      {
+        mode = "n";
+        action = "<cmd>lua require('persistence').stop()<CR>";
+        key = "<leader>qd";
+        options = {
+          desc = "Stop persistence";
+        };
+      }
+    ];
     plugins.persistence = {
       enable = true;
       settings = {
