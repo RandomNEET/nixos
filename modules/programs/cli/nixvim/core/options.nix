f433542c27b3a8125c4c6179f5d90849ea4abdec
RandomNEET <dev@randomneet.me>
2026-01-02 14:43:26 +0800

    overwrite clipboard in ssh

--- modules/programs/cli/nixvim/core/options.nix
+++ modules/programs/cli/nixvim/core/options.nix
@@ -20,6 +20,31 @@
       diagnostic.settings = {
         virtual_text = false;
       };
+      extraConfigLua = ''
+        local function is_ssh()
+        	return vim.env.SSH_CONNECTION or vim.env.SSH_CLIENT or vim.env.SSH_TTY
+        end
+
+        if is_ssh() then
+        	local function paste()
+        		return {
+        			vim.fn.split(vim.fn.getreg(""), "\n"),
+        			vim.fn.getregtype(""),
+        		}
+        	end
+        	vim.g.clipboard = {
+        		name = "OSC 52",
+        		copy = {
+        			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+        		},
+        		paste = {
+        			["+"] = paste,
+        			["*"] = paste,
+        		},
+        	}
+        end
+      '';
     };
   };
 }
