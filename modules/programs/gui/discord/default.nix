fcb409de7a8682c5a1fd9817914ede84e796cdf2
RandomNEET <dev@randomneet.me>
2026-01-17 18:24:26 +0800

    update vesktop config

--- modules/programs/gui/discord/default.nix
+++ modules/programs/gui/discord/default.nix
@@ -4,9 +4,35 @@
       programs.vesktop = {
         enable = true;
         settings = {
-          notifyAboutUpdates = true;
-          autoUpdate = false;
-          autoUpdateNotification = false;
+          appBadge = false;
+          arRPC = true;
+          checkUpdates = false;
+          customTitleBar = false;
+          disableMinSize = true;
+          minimizeToTray = false;
+          tray = false;
+          splashBackground = "#000000";
+          splashColor = "#ffffff";
+          splashTheming = true;
+          staticTitle = true;
+          hardwareAcceleration = true;
+          discordBranch = "stable";
+        };
+        vencord = {
+          settings = {
+            autoUpdate = false;
+            autoUpdateNotification = false;
+            notifyAboutUpdates = false;
+            useQuickCss = true;
+            disableMinSize = true;
+            plugins = {
+              MessageLogger = {
+                enabled = true;
+                ignoreSelf = true;
+              };
+              FakeNitro.enabled = true;
+            };
+          };
         };
       };
     }
