69c16753367f077879f9c8800ab007c791666c5d
RandomNEET <dev@randomneet.me>
2026-03-25 23:25:13 +0800

    wayland-pipewire-idle-inhibit: add config.toml

--- modules/desktop/shared/services/wayland-pipewire-idle-inhibit/default.nix
+++ modules/desktop/shared/services/wayland-pipewire-idle-inhibit/default.nix
@@ -1,19 +1,39 @@
-{ pkgs, ... }:
+{ pkgs, opts, ... }:
 {
-  systemd.user = {
-    services.wayland-pipewire-idle-inhibit = {
-      description = "Inhibit Wayland idling when media is played through pipewire";
-      after = [
-        "pipewire.service"
-        "graphical-session.target"
-      ];
-      wants = [ "pipewire.service" ];
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.wayland-pipewire-idle-inhibit}/bin/wayland-pipewire-idle-inhibit -d 5";
-        Restart = "always";
-        RestartSec = 10;
+  home-manager.sharedModules = [
+    {
+      systemd.user = {
+        services.wayland-pipewire-idle-inhibit = {
+          Unit = {
+            Description = "Inhibit Wayland idling when media is played through pipewire";
+            Documentation = "https://github.com/rafaelrc7/wayland-pipewire-idle-inhibit";
+            After = [
+              "pipewire.service"
+              "graphical-session.target"
+            ];
+            Wants = [ "pipewire.service" ];
+          };
+          Service = {
+            ExecStart = "${pkgs.wayland-pipewire-idle-inhibit}/bin/wayland-pipewire-idle-inhibit";
+            Restart = "always";
+            RestartSec = 10;
+          };
+          Install = {
+            WantedBy = [ "graphical-session.target" ];
+          };
+        };
       };
-    };
-  };
+      xdg.configFile."wayland-pipewire-idle-inhibit/config.toml".text = ''
+        verbosity = "WARN"
+        media_minimum_duration = 5
+        idle_inhibitor = "wayland"
+        sink_whitelist = ${
+          builtins.toJSON ([ ] ++ (opts.wayland-pipewire-idle-inhibit.sink_whitelist or [ ]))
+        }
+        node_blacklist = ${
+          builtins.toJSON ([ ] ++ (opts.wayland-pipewire-idle-inhibit.node_blacklist or [ ]))
+        }
+      '';
+    }
+  ];
 }
