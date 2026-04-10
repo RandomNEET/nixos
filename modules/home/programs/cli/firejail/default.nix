271c06dfc442b26f51660525da5c26ca13e4b9dc
RandomNEET <dev@randomneet.me>
2026-04-10 17:40:33 +0800

    firejail: refactor

--- modules/home/programs/cli/firejail/default.nix
+++ modules/home/programs/cli/firejail/default.nix
@@ -1,5 +1,4 @@
 {
-  osConfig,
   config,
   lib,
   pkgs,
@@ -7,7 +6,6 @@
 }:
 let
   inherit (lib) getBin;
-
   profiles = import ./profiles {
     inherit
       config
@@ -15,133 +13,122 @@ let
       pkgs
       ;
   };
-
-  check =
+  checkPkgs =
     pname: builtins.any (p: (builtins.isAttrs p) && (lib.getName p == pname)) config.home.packages;
-
-  packages = {
-    aerc = {
-      enable = config.programs.aerc.enable;
-      bin = "${getBin pkgs.aerc}/bin/aerc";
-      profile = profiles.aerc;
-    };
-    chromium = {
-      enable = config.programs.chromium.enable;
-      bin = "${config.programs.chromium.finalPackage}/bin/chromium";
-      profile = profiles.chromium;
-    };
-    firefox = {
-      enable = config.programs.firefox.enable;
-      bin = "${config.programs.firefox.finalPackage}/bin/firefox";
-      profile = profiles.firefox;
-    };
-    mpv = {
-      enable = config.programs.mpv.enable;
-      bin = "${config.programs.mpv.finalPackage}/bin/mpv";
-      profile = profiles.mpv;
-    };
-    newsboat = {
-      enable = config.programs.newsboat.enable;
-      bin = "${getBin pkgs.newsboat}/bin/newsboat";
-      profile = profiles.newsboat;
-    };
-    obsidian = {
-      enable = config.programs.obsidian.enable;
-      bin = "${getBin pkgs.obsidian}/bin/obsidian";
-      profile = profiles.obsidian;
-    };
-    qutebrowser = {
-      enable = config.programs.qutebrowser.enable;
-      bin = "${getBin pkgs.qutebrowser}/bin/qutebrowser";
-      profile = profiles.qutebrowser;
-    };
-    spotify = {
-      enable = (config.programs ? spicetify && config.programs.spicetify.enable);
-      bin = "${config.programs.spicetify.spicedSpotify}/bin/spotify";
-      profile = profiles.spotify;
-    };
-    thunderbird = {
-      enable = config.programs.thunderbird.enable;
-      bin = "${getBin pkgs.thunderbird}/bin/thunderbird";
-      profile = profiles.thunderbird;
-    };
-    vesktop = {
-      enable = config.programs.vesktop.enable;
-      bin = "${getBin pkgs.vesktop}/bin/vesktop";
-      profile = profiles.vesktop;
-    };
-    yt-dlp = {
-      enable = config.programs.yt-dlp.enable;
-      bin = "${getBin pkgs.yt-dlp}/bin/yt-dlp";
-      profile = profiles.yt-dlp;
-    };
-    zathura = {
-      enable = config.programs.zathura.enable;
-      bin = "${getBin pkgs.zathura}/bin/zathura";
-      profile = profiles.zathura;
-    };
-
-    calibre = {
-      enable = check "calibre";
-      bin = "${getBin pkgs.calibre}/bin/calibre";
-      profile = profiles.calibre;
-    };
-    gimp = {
-      enable = check "gimp";
-      bin = "${getBin pkgs.gimp}/bin/gimp";
-      profile = profiles.gimp;
-    };
-    libreoffice = {
-      enable = check "libreoffice";
-      bin = "${getBin pkgs.libreoffice}/bin/libreoffice";
-      profile = profiles.libreoffice;
-    };
-    qbittorrent = {
-      enable = check "qbittorrent";
-      bin = "${getBin pkgs.qbittorrent}/bin/qbittorrent";
-      profile = profiles.qbittorrent;
-    };
-    qq = {
-      enable = check "qq";
-      bin = "${getBin pkgs.qq}/bin/qq";
-      profile = profiles.qq;
-    };
-    tor-browser = {
-      enable = check "tor-browser";
-      bin = "${getBin pkgs.tor-browser}/bin/tor-browser";
-      profile = profiles.tor-browser;
-    };
-    w3m = {
-      enable = check "w3m";
-      bin = "${getBin pkgs.w3m}/bin/w3m";
-      profile = profiles.w3m;
-    };
-  };
-
-  toWarp = lib.filter (name: packages.${name}.enable) (builtins.attrNames packages);
-
-  # line '2> >(grep -v -e "fseccomp" -e "dumpable" >&2)' suppresses known warnings about 'dumpable process' and 'fseccomp' permissions
-  # known issue caused by this line: unable to launch w3m in newsboat
-  warpCmd =
-    name:
-    let
-      pkg = packages.${name};
-    in
-    ''
-      #! ${pkgs.bash}/bin/bash -e
-      exec /run/wrappers/bin/firejail \
-        --profile=${pkg.profile} \
-        -- ${pkg.bin} "$@" \
-        2> >(grep -v -e "fseccomp" -e "dumpable" >&2)
-    '';
 in
 {
-  home = lib.mkIf osConfig.programs.firejail.enable {
-    file = lib.genAttrs toWarp (name: {
-      target = ".local/bin/${name}";
-      executable = true;
-      text = warpCmd name;
-    });
-    sessionPath = [ "$HOME/.local/bin" ];
+  imports = [ ./options.nix ];
+  programs.firejail = {
+    enable = true;
+    wrappedBinaries = {
+      aerc = {
+        enable = config.programs.aerc.enable;
+        executable = "${getBin pkgs.aerc}/bin/aerc";
+        profile = profiles.aerc;
+        filterStderr.enable = true;
+      };
+      chromium = {
+        enable = config.programs.chromium.enable;
+        executable = "${config.programs.chromium.finalPackage}/bin/chromium";
+        profile = profiles.chromium;
+      };
+      firefox = {
+        enable = config.programs.firefox.enable;
+        executable = "${config.programs.firefox.finalPackage}/bin/firefox";
+        profile = profiles.firefox;
+      };
+      mpv = {
+        enable = config.programs.mpv.enable;
+        executable = "${config.programs.mpv.finalPackage}/bin/mpv";
+        profile = profiles.mpv;
+        filterStderr = {
+          enable = true;
+          patterns = [
+            "dumpable"
+            "fseccomp"
+            "fsec-optimize"
+          ];
+        };
+      };
+      newsboat = {
+        enable = config.programs.newsboat.enable;
+        executable = "${getBin pkgs.newsboat}/bin/newsboat";
+        profile = profiles.newsboat;
+        filterStderr.enable = true;
+      };
+      obsidian = {
+        enable = config.programs.obsidian.enable;
+        executable = "${getBin pkgs.obsidian}/bin/obsidian";
+        profile = profiles.obsidian;
+      };
+      qutebrowser = {
+        enable = config.programs.qutebrowser.enable;
+        executable = "${getBin pkgs.qutebrowser}/bin/qutebrowser";
+        profile = profiles.qutebrowser;
+      };
+      spotify = {
+        enable = config.programs.spicetify.enable;
+        executable = "${config.programs.spicetify.spicedSpotify}/bin/spotify";
+        profile = profiles.spotify;
+      };
+      thunderbird = {
+        enable = config.programs.thunderbird.enable;
+        executable = "${getBin pkgs.thunderbird}/bin/thunderbird";
+        profile = profiles.thunderbird;
+      };
+      vesktop = {
+        enable = config.programs.vesktop.enable;
+        executable = "${getBin pkgs.vesktop}/bin/vesktop";
+        profile = profiles.vesktop;
+      };
+      yt-dlp = {
+        enable = config.programs.yt-dlp.enable;
+        executable = "${getBin pkgs.yt-dlp}/bin/yt-dlp";
+        profile = profiles.yt-dlp;
+        filterStderr.enable = true;
+      };
+      zathura = {
+        enable = config.programs.zathura.enable;
+        executable = "${getBin pkgs.zathura}/bin/zathura";
+        profile = profiles.zathura;
+      };
+
+      calibre = {
+        enable = checkPkgs "calibre";
+        executable = "${getBin pkgs.calibre}/bin/calibre";
+        profile = profiles.calibre;
+      };
+      gimp = {
+        enable = checkPkgs "gimp";
+        executable = "${getBin pkgs.gimp}/bin/gimp";
+        profile = profiles.gimp;
+      };
+      libreoffice = {
+        enable = checkPkgs "libreoffice";
+        executable = "${getBin pkgs.libreoffice}/bin/libreoffice";
+        profile = profiles.libreoffice;
+      };
+      qbittorrent = {
+        enable = checkPkgs "qbittorrent";
+        executable = "${getBin pkgs.qbittorrent}/bin/qbittorrent";
+        profile = profiles.qbittorrent;
+      };
+      qq = {
+        enable = checkPkgs "qq";
+        executable = "${getBin pkgs.qq}/bin/qq";
+        profile = profiles.qq;
+      };
+      tor-browser = {
+        enable = checkPkgs "tor-browser";
+        executable = "${getBin pkgs.tor-browser}/bin/tor-browser";
+        profile = profiles.tor-browser;
+      };
+      w3m = {
+        enable = checkPkgs "w3m";
+        executable = "${getBin pkgs.w3m}/bin/w3m";
+        profile = profiles.w3m;
+        filterStderr.enable = true;
+      };
+    };
   };
 }
---
+++ modules/home/programs/cli/firejail/options.nix
@@ -0,0 +1,144 @@
+{
+  osConfig,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkOption
+    mkEnableOption
+    types
+    mkMerge
+    mkIf
+    mkForce
+    ;
+
+  cfg = config.programs.firejail;
+
+  enabledBinaries = lib.filterAttrs (
+    name: value: if builtins.isAttrs value then value.enable else true
+  ) cfg.wrappedBinaries;
+
+  wrappedBins =
+    pkgs.runCommand "hm-firejail-wrapped-binaries"
+      {
+        preferLocalBuild = true;
+        allowSubstitutes = false;
+        meta.priority = -1;
+      }
+      ''
+        mkdir -p $out/bin
+        mkdir -p $out/share/applications
+
+        ${lib.concatStringsSep "\n" (
+          lib.mapAttrsToList (
+            command: value:
+            let
+              opts =
+                if builtins.isAttrs value then
+                  value
+                else
+                  {
+                    enable = true;
+                    executable = value;
+                    profile = null;
+                    desktop = null;
+                    extraArgs = [ ];
+                    filterStderr = {
+                      enable = false;
+                      patterns = [ ];
+                    };
+                  };
+
+              args = lib.escapeShellArgs (
+                opts.extraArgs ++ (lib.optional (opts.profile != null) "--profile=${toString opts.profile}")
+              );
+
+              # This filter suppresses known warnings
+              grepPatterns = lib.concatMapStrings (p: " -e ${lib.escapeShellArg p}") opts.filterStderr.patterns;
+              stderrFilter =
+                if opts.filterStderr.enable && (opts.filterStderr.patterns != [ ]) then
+                  "2> >(${pkgs.gnugrep}/bin/grep -v ${grepPatterns} >&2)"
+                else
+                  "";
+            in
+            ''
+              cat <<_EOF >$out/bin/${command}
+              #! ${pkgs.bash}/bin/bash -e
+              exec /run/wrappers/bin/firejail ${args} -- ${toString opts.executable} "\$@" ${stderrFilter}
+              _EOF
+              chmod 0755 $out/bin/${command}
+              ${lib.optionalString (opts.desktop != null) ''
+                if [ -f "${opts.desktop}" ]; then
+                  substitute "${opts.desktop}" "$out/share/applications/$(basename ${opts.desktop})" \
+                    --replace "${opts.executable}" "$out/bin/${command}"
+                fi
+              ''}
+            ''
+          ) enabledBinaries
+        )}
+      '';
+in
+{
+  options = {
+    programs.firejail = {
+      enable = mkEnableOption "firejail, a sandboxing tool for Linux";
+      wrappedBinaries = mkOption {
+        type = types.attrsOf (
+          types.either types.path (
+            types.submodule {
+              options = {
+                enable = mkOption {
+                  type = types.bool;
+                  default = true;
+                  description = "Whether to enable this specific binary wrapper. Useful for conditional sandboxing.";
+                };
+                executable = mkOption {
+                  type = types.path;
+                  description = "The absolute path to the executable to be sandboxed.";
+                };
+                profile = mkOption {
+                  type = types.nullOr types.path;
+                  default = null;
+                  description = "The firejail profile to use for this application.";
+                };
+                desktop = mkOption {
+                  type = types.nullOr types.path;
+                  default = null;
+                  description = "The .desktop file to modify. The module will replace the absolute path to the executable with the firejail wrapper path.";
+                };
+                extraArgs = mkOption {
+                  type = types.listOf types.str;
+                  default = [ ];
+                  description = "Extra command-line arguments to pass to firejail.";
+                };
+                filterStderr = {
+                  enable = mkEnableOption "stderr filtering for this binary";
+                  patterns = mkOption {
+                    type = types.listOf types.str;
+                    default = [
+                      "dumpable"
+                      "fseccomp"
+                    ];
+                    description = "List of patterns (strings) to filter out from stderr using grep -v.";
+                  };
+                };
+              };
+            }
+          )
+        );
+        default = { };
+        description = "Attribute set of binaries to wrap with firejail and add to the user profile.";
+      };
+    };
+  };
+
+  config = mkMerge [
+    { programs.firejail.enable = mkForce osConfig.programs.firejail.enable; }
+    (mkIf cfg.enable {
+      home.packages = [ wrappedBins ];
+    })
+  ];
+}
